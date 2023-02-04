b1a62127ce5b1bcd57a3159d4db44151697d087f
Benneth93 <58491492+Benneth93@users.noreply.github.com>
2023-02-04 02:45:36 +0000

    Add Pass percentage indicator

--- Pages/TestHistory.cshtml.cs
+++ Pages/TestHistory.cshtml.cs
@@ -1,4 +1,5 @@
 using ISTQBQuiz.Dtos;
+using ISTQBQuiz.Models;
 using ISTQBQuiz.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,9 +8,13 @@ namespace ISTQBQuiz.Pages;
 public class TestHistory : PageModel
 {
     public List<TestResultDto> _testHistory;
+    public double _passPercentage;
+    public double _failPercentage;
     public void OnGet()
     {
         TestResultService.GetTestResults();
         _testHistory = TestResultService._testResults;
+        _passPercentage = TestResultService._passPercentage;
+        _failPercentage = TestResultService._failPercentage;
     }
 }
--- Services/TestResultService.cs
+++ Services/TestResultService.cs
@@ -1,11 +1,14 @@
 using ISTQBQuiz.Dtos;
+using ISTQBQuiz.Models;
 
 namespace ISTQBQuiz.Services;
 
 public static class TestResultService
 {
     public static List<TestResultDto> _testResults { get; set; }
-
+    public static double _passPercentage { get; set; }
+    public static double _failPercentage { get; set; }
+    
     static TestResultService()
     {
         _testResults = new();
@@ -15,5 +18,16 @@ public static class TestResultService
     {
         var quizDbService = new QuizDatabaseService();
         _testResults = quizDbService.GetTestResults();
+        CalculatePassPercentage();
+    }
+
+    private static void CalculatePassPercentage()
+    {
+        var numberOfPassResults = _testResults.Count(result => result.Result == "PASS");
+        var numberOfFailResults = _testResults.Count(result => result.Result == "FAIL");
+
+        _passPercentage = (numberOfPassResults / _testResults.Count) * 100;
+        _failPercentage = (numberOfFailResults / _testResults.Count) * 100;
+        
     }
 }
